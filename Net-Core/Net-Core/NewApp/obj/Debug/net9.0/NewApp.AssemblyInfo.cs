9d5c9c1ed000a9882a4b923cf30dde69b4d76cf0
BuiVanNam9 <nam833909@gmail.com>
2025-08-23 02:10:30 +0700

    DemoMVC

--- Net-Core/Net-Core/NewApp/obj/Debug/net9.0/NewApp.AssemblyInfo.cs
+++ Net-Core/Net-Core/NewApp/obj/Debug/net9.0/NewApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NewApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13f2db5f42389c367521ab1f82929eb7e8fb3d28")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f62ee217e67859dc6415ae244e106ab70f793104")]
 [assembly: System.Reflection.AssemblyProductAttribute("NewApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NewApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
